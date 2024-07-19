e39d2437a1e492fd1c69dd80296282369a21e2ad
hoangtrong <hoangcongtrong054@gmail.com>
2024-07-19 09:22:17 +0700

    Update code fe to InputSection

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,5 @@
-export default function Home() {
-  return (
-    <div className="bg-white min-h-screen text-black">
-      Home page
-    </div>
-  );
-}
+"use client";
+
+import { useState } from "react";
+import InputSection from "@/components/InputSection";
+
